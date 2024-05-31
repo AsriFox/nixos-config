a12acc042bcf11cb8971da8f90e58cb735bccdc1
AsriFox <asrifox@yandex.ru>
2024-05-31 22:07:23 +0300

    Add helix editor

--- modules/cli-tools.nix
+++ modules/cli-tools.nix
@@ -1,5 +1,5 @@
 { lib, pkgs, ... }: {
-  home.packages = with pkgs; [ wl-clipboard neovim nodejs gcc nixfmt-classic ];
+  home.packages = with pkgs; [ wl-clipboard nodejs gcc nixfmt-classic helix ];
 
   home.sessionVariables = {
     XCURSOR_SIZE = 32;
