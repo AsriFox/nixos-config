0930dc38c3e04ea3eb2bcfe4f865cbf656ea9edf
AsriFox <asrifox@yandex.ru>
2024-07-02 20:49:08 +0300

    Add docker

--- hosts/tower-nix/default.nix
+++ hosts/tower-nix/default.nix
@@ -3,6 +3,14 @@
 
   networking.hostName = "tower-nix";
 
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+  };
+
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
